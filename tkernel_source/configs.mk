e71b9536d6bee2b1b64f765e1cf46470c66c055c
Du Huanpeng <u74147@gmail.com>
2016-02-13 20:41:57 +0800

    script: configs.mk
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

--- tkernel_source/configs.mk
+++ tkernel_source/configs.mk
@@ -5,11 +5,31 @@
 
 
 export BD=${PWD}
-export GNU_BD="/opt/gcc-arm/"
-export GNUarm_2="/opt/gcc-arm/"
+export GNU_BD="/opt/gcc-arm"
+export GNUarm_2="/opt/gcc-arm/."
 export BUILD_TARGET="tef_em1d"
 
 alias bd='cd ${BD}'
 alias tk='cd ${BD}"/kernel/sysmain/build/tef_em1d/"'
 alias tm='cd ${BD}"/monitor/tmmain/build/tef_em1d/"'
+alias ro='cd ${BD}"/config/build/tef_em1d/"'
+
+tk-link ()
+{
+	CROSS=${1}
+	ln -s ${CROSS}ar ar
+	ln -s ${CROSS}objcopy arm_2-unknown-tkernel-objcopy
+	ln -s ${CROSS}gcc gcc4arm
+	ln -s ${CROSS}nm  nm
+	ln -s ${CROSS}ranlib  ranlib
+}
+
+tk-build ()
+{
+	cd ${BD}
+
+	make -C kernel/sysmain/build/tef_em1d/
+	make -C config/build/tef_em1d/
+	make -C monitor/tmmain/build/tef_em1d/
+}
 
