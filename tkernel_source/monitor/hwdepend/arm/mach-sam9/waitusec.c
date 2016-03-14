c815201f5687b6d0db12b0af90a52347aa17431b
Du Huanpeng <u74147@gmail.com>
2016-03-14 14:54:34 +0800

    ports-arm: sam9, no precise wait
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

--- tkernel_source/monitor/hwdepend/arm/mach-sam9/waitusec.c
+++ tkernel_source/monitor/hwdepend/arm/mach-sam9/waitusec.c
@@ -60,46 +60,7 @@ EXPORT	void	waitMsec(UW msec)
  */
 EXPORT	void	setupWaitUsec(void)
 {
-	UW	t0, t1, t2;
-
-#define	MAX_CNT		(ACPU_CLK * 64 / 10)	/* 1 Clock */
-#define	MIN_CNT		(ACPU_CLK * 64 / 1280)	/* 128 Clock */
-
-        /* use TI0 timer, and assume clock is PLL3 / 8 */
-			/* Timer stop, count clear */
-	
-
-	/* maximum count */
-			/* Timer start */
-
-	delay64us = 64;
-	/* wait for a while until things settle down */
-
-
-				/* Timer stop, count clear */
-	
-	
-		/* count for 3000 times */
-		/* count for 1000 times */
-		/* count for 2000 times */
-
-	/*
-         * calculate the count for 64 microsec
-	 *
-         *                    2000 loops x timer clock [MHz] x 64 [microsec]
-	 *	delay64us = ------------------------------------------------
-	 *	                                 t2
-	 *
-         * * considering the representation of PLL3_CLK (1/1000MHz unit), and setting of pre scaler,
-         * it can be written down as follows.
-	 *
-         *                    2 loops x PLL3_CLK [1/1000MHz] x 8 [microsec]
-	 *	delay64us = -------------------------------------------
-	 *	                                 t2
-	 *
-	 */
-	 
-
+	delay64us = 64*2;
 	return;
 }
 
