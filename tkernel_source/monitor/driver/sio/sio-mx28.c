80f9eb52d579f228a8c467d1a550c1bc27b219ee
Du Huanpeng <u74147@gmail.com>
2016-03-22 23:19:04 +0800

    ports-arm: add i.MX28x dummy support
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

---
+++ tkernel_source/monitor/driver/sio/sio-mx28.c
@@ -0,0 +1,216 @@
+/*
+ *----------------------------------------------------------------------
+ *    T-Kernel 2.0 Software Package
+ *
+ *    Copyright 2011 by Ken Sakamura.
+ *    This software is distributed under the latest version of T-License 2.x.
+ *----------------------------------------------------------------------
+ *
+ *    Released by T-Engine Forum(http://www.t-engine.org/) at 2011/05/17.
+ *    Modified by T-Engine Forum at 2013/02/28.
+ *    Modified by TRON Forum(http://www.tron.org/) at 2015/06/01.
+ *
+ *----------------------------------------------------------------------
+ *	Copyright (C) 2014, 2015, 2016 Du Huanpeng<u74147@gmail.com>
+ */
+
+/*
+ *	base on monitor/driver/sio/src/ns16550.c
+ *	sio-mx28.c
+ *
+ *       serial port I/O
+ */
+
+#include <tmonitor.h>
+#include <mach/em1d512.h>
+
+
+#define AT91C_BASE_DBGU		0xFFFFEE00 // (DBGU) Base Address
+#define DBGU_CR          	0x00000000 // (DBGU_CR) Control Register
+#define DBGU_MR          	0x00000004 // (DBGU_MR) Mode Register
+#define DBGU_IER         	0x00000008 // (DBGU_IER) Interrupt Enable Register
+#define DBGU_IDR         	0x0000000C // (DBGU_IDR) Interrupt Disable Register
+#define DBGU_IMR         	0x00000010 // (DBGU_IMR) Interrupt Mask Register
+#define DBGU_CSR         	0x00000014 // (DBGU_CSR) Channel Status Register
+#define DBGU_RHR         	0x00000018 // (DBGU_RHR) Receiver Holding Register
+#define DBGU_THR         	0x0000001C // (DBGU_THR) Transmitter Holding Register
+#define DBGU_BRGR        	0x00000020 // (DBGU_BRGR) Baud Rate Generator Register
+#define DBGU_CIDR        	0x00000040 // (DBGU_CIDR) Chip ID Register
+#define DBGU_EXID        	0x00000044 // (DBGU_EXID) Chip ID Extension Register
+#define DBGU_FNTR        	0x00000048 // (DBGU_FNTR) Force NTRST Register
+
+#define DBGU_PTCR		0x00000120
+
+
+#define AT91C_US_PAR          (0x7 <<  9) // (DBGU) Parity type
+#define 	AT91C_US_PAR_EVEN                 (0x0 <<  9) // (DBGU) Even Parity
+#define 	AT91C_US_PAR_ODD                  (0x1 <<  9) // (DBGU) Odd Parity
+#define 	AT91C_US_PAR_SPACE                (0x2 <<  9) // (DBGU) Parity forced to 0 (Space)
+#define 	AT91C_US_PAR_MARK                 (0x3 <<  9) // (DBGU) Parity forced to 1 (Mark)
+#define 	AT91C_US_PAR_NONE                 (0x4 <<  9) // (DBGU) No Parity
+#define 	AT91C_US_PAR_MULTI_DROP           (0x6 <<  9) // (DBGU) Multi-drop mode
+
+// -------- DBGU_CR : (DBGU Offset: 0x0) Debug Unit Control Register -------- 
+#define AT91C_US_RSTRX        (0x1 <<  2) // (DBGU) Reset Receiver
+#define AT91C_US_RSTTX        (0x1 <<  3) // (DBGU) Reset Transmitter
+#define AT91C_US_RXEN         (0x1 <<  4) // (DBGU) Receiver Enable
+#define AT91C_US_RXDIS        (0x1 <<  5) // (DBGU) Receiver Disable
+#define AT91C_US_TXEN         (0x1 <<  6) // (DBGU) Transmitter Enable
+#define AT91C_US_TXDIS        (0x1 <<  7) // (DBGU) Transmitter Disable
+#define AT91C_US_RSTSTA       (0x1 <<  8) // (DBGU) Reset Status Bits
+
+// -------- PDC_PTCR : (PDC Offset: 0x20) PDC Transfer Control Register -------- 
+#define AT91C_PDC_RXTEN       (0x1 <<  0) // (PDC) Receiver Transfer Enable
+#define AT91C_PDC_RXTDIS      (0x1 <<  1) // (PDC) Receiver Transfer Disable
+#define AT91C_PDC_TXTEN       (0x1 <<  8) // (PDC) Transmitter Transfer Enable
+#define AT91C_PDC_TXTDIS      (0x1 <<  9) // (PDC) Transmitter Transfer Disable
+// -------- PDC_PTSR : (PDC Offset: 0x24) PDC Transfer Status Register -------- 
+
+
+/// Frequency of the board main oscillator.
+#define BOARD_MAINOSC           12000000
+
+/// Master clock frequency (when using board_lowlevel.c).
+// ((12MHz / DIVA / PLLADIV2 / MDIV) * (MULA+1)
+#define BOARD_MCK               ((unsigned long)((BOARD_MAINOSC / 3 / 2 / 3) * 200 )) // 133MHz
+//#define BOARD_MCK               ((unsigned long)((BOARD_MAINOSC / 3 / 2 / 4) * 200 )) // 100MHz
+
+
+/*
+ * serial port hardware configuration definition
+ */
+typedef struct {
+	UW	iob;		/* I/O base address */
+} DEFSIO;
+
+LOCAL const DEFSIO	DefSIO[] = {
+	{ 0 },
+};
+
+/* ------------------------------------------------------------------------ */
+#define	N_DEFSIO	( sizeof(DefSIO) / sizeof(DEFSIO) )
+
+/*
+ * serial port I/O
+ */
+void putSIO_mx28( SIOCB *scb, UB c )
+{
+#ifdef __GNUC__
+	unsigned val;
+check_ready:
+	val = in_w(AT91C_BASE_DBGU+0x14);
+	val &= (1<<9);
+	if(val == 0) goto check_ready;
+#else
+	while (in_w(AT91C_BASE_DBGU+0x14) & (1<<9) == 0);
+#endif
+	out_b(AT91C_BASE_DBGU+0x1C, c);
+
+#ifdef __GNUC__
+wait_done:
+	val = in_w(AT91C_BASE_DBGU+0x14);
+	val &= (1<<9);
+	if(val == 0) goto wait_done;
+#else
+	while (in_w(AT91C_BASE_DBGU+0x14) & (1<<9) == 0);
+#endif
+
+
+
+	return;
+}
+
+/*
+ * serial port input
+ *       tmo     timeout (milliseconds)
+ *              You can not wait forever.
+ *       return value       >= 0 : character code
+ *                 -1 : timeout
+ *       input data using buffer.
+ *       receive error is ignored.
+ */
+LOCAL W getSIO_mx28(SIOCB *scb, W tmo )
+{
+	W	sts, c = 0;
+
+	tmo *= 1000/20; 	/* convert tmo to 20 usec units */
+
+	/* receive as much data as possible in the receive buffer */
+	while (scb->iptr - scb->optr < SIO_RCVBUFSZ) {
+
+		sts = in_w(AT91C_BASE_DBGU + 0x14);
+
+
+		/* is there data in FIFO? */
+		if ( sts & 1 == 0) {
+			if (scb->iptr != scb->optr) break;  /* already received */
+			if (tmo-- <= 0) break;		    /* timeout */
+			waitUsec(20);
+			continue;
+		}
+
+		/* receive data input */
+		if (sts & 1) c = in_b(AT91C_BASE_DBGU + 0x18);
+
+
+		/* set data to rcvbuf */
+		scb->rcvbuf[scb->iptr++ & SIO_PTRMSK] = c;
+	}
+
+	/* return the data in rcvbuf */
+	return (scb->iptr == scb->optr)?
+			-1 : scb->rcvbuf[scb->optr++ & SIO_PTRMSK];
+}
+
+/* ------------------------------------------------------------------------ */
+
+/*
+ * initialize serial port
+ *       serial port that is supported by the initialization of CFGSIO
+ *       speed   communication speed (bps)
+ *       initialize the serial port according to the specified parameters and set SIOCB
+ *       SIOCB is given in 0-cleared state initially.
+ *       Subsequent I/O operations uses the SIOCB.
+ *
+ *       Only for PC/AT version
+ *      if speed = 0, we use the value in biosp->siomode.
+ *       But we use only the transmission speed and other settings are ignored.
+ *       Efforts were made to be compatible B-right/V, but because of the ignorance of no-speed settings such as data length and stop bit length,
+ *       we have reduced functionality.
+ */
+EXPORT ER initSIO_mx28(SIOCB *scb, const CFGSIO *csio, W baudrate)
+{
+	W mode = AT91C_US_PAR_NONE;
+	W mck  = BOARD_MCK;
+
+	if ( (UW)csio->info >= N_DEFSIO ) return E_PAR;
+
+        /* select the target port */
+	scb->info = DefSIO[csio->info].iob;
+	strncpy(scb->name, "mx28-sio", sizeof(scb->name));
+
+        /* initialize serial controller */
+	// Reset & disable receiver and transmitter, disable interrupts
+	out_w(AT91C_BASE_DBGU + DBGU_CR, AT91C_US_RSTRX | AT91C_US_RSTTX);
+	out_w(AT91C_BASE_DBGU + DBGU_IDR, 0xFFFFFFFF);
+
+	// Configure baud rate
+	out_w(AT91C_BASE_DBGU + DBGU_BRGR,  mck / (baudrate * 16));
+
+	// Configure mode register
+	out_w(AT91C_BASE_DBGU + DBGU_MR,  mode);
+
+	// Disable DMA channel
+	out_w(AT91C_BASE_DBGU + DBGU_PTCR, AT91C_PDC_RXTDIS | AT91C_PDC_TXTDIS);
+
+	// Enable receiver and transmitter
+	out_w(AT91C_BASE_DBGU + DBGU_CR, AT91C_US_RXEN | AT91C_US_TXEN);
+
+
+	/* I/O function default */
+	scb->put = putSIO_mx28;
+	scb->get = getSIO_mx28;
+
+
+	return E_OK;
+}
---
+++ tkernel_source/monitor/hwdepend/arm/mach-mx28/system.c
@@ -0,0 +1,279 @@
+/*
+ *----------------------------------------------------------------------
+ *    T-Kernel 2.0 Software Package
+ *
+ *    Copyright 2011 by Ken Sakamura.
+ *    This software is distributed under the latest version of T-License 2.x.
+ *----------------------------------------------------------------------
+ *
+ *    Released by T-Engine Forum(http://www.t-engine.org/) at 2011/05/17.
+ *    Modified by T-Engine Forum at 2011/09/08.
+ *    Modified by T-Engine Forum at 2013/03/04.
+ *    Modified by TRON Forum(http://www.tron.org/) at 2015/06/01.
+ *
+ *----------------------------------------------------------------------
+ */
+
+/*
+ *	system.c
+ *
+ *       system-related processing
+ *
+ *       target: EM1D-512
+ */
+
+#include "sysdepend.h"
+#include <mach/em1d512.h>
+
+EXPORT	UW	DipSw;		/* DipSw status */
+
+/* hardware dependent functions */
+IMPORT	UW	DipSwStatus(void);
+IMPORT	void	usbPower(BOOL power);
+IMPORT	void	powerOff(void);
+IMPORT	void	resetStart(void);
+
+/* interrupt entry point (eitent.S) */
+LOCAL void	_gio0Hdr(void) {}
+LOCAL void	_gio1Hdr(void) {}
+LOCAL void	_gio2Hdr(void) {}
+LOCAL void	_gio3Hdr(void) {}
+LOCAL void	_gio4Hdr(void) {}
+LOCAL void	_gio5Hdr(void) {}
+LOCAL void	_gio6Hdr(void) {}
+LOCAL void	_gio7Hdr(void) {}
+
+/* default handler (cmdsvc) */
+IMPORT	void	_defaultHdr(void);
+
+/* macros for manipulating cache/MMU/PMIC */
+#define	EnbCacheMMU(x)	setCacheMMU(ENB_CACHEMMU)
+#define	DisCacheMMU(x)	setCacheMMU(ENB_MMUONLY) /* MMU can't be turned off */
+
+/* ------------------------------------------------------------------------ */
+
+IMPORT	char	__loadaddr;	/* monitor load address */
+IMPORT	MEMSEG	NoMemSeg[];	/* memory unused area */
+IMPORT	W	N_NoMemSeg;
+
+/*
+        power management controller (DA9052) handler routines
+*/
+#define	SPIPol	0x0092
+
+/* initialize SPI for PMIC communication */
+LOCAL	void	pmicInit(void)
+{
+#if 0
+	out_w(SPn_MODE(SP0), 0x2700);		/* 8bit, CS0, Master, CPU mode */
+	out_w(SPn_TIECS(SP0), 0x000f);		/* CS0: follow the specification by SPn_POL */
+	out_w(SPn_POL(SP0), SPIPol);
+	out_w(SPn_ENCLR(SP0), ~0);		/* interrupt disable */
+
+	out_w(SPn_CONTROL(SP0), 0x0100);	/* start reset */
+	waitUsec(10);
+	out_w(SPn_CONTROL(SP0), 0x0000);	/* release reset */
+	out_w(SPn_CONTROL2(SP0), 0x0000);
+#else
+	putString("[driver]: pmicInit\n");
+#endif
+
+	return;
+}
+
+/* wait for data of SPI for PMIC communication */
+LOCAL	void	pmicWait(void)
+{
+#if 0
+	W	i;
+
+	for (i = 1000000; i > 0; i--) {
+		if (in_w(SPn_RAW_STATUS(SP0)) & 0x0004) break;
+		waitUsec(1);
+	}
+	if (!i) pmicInit();
+#else
+	putString("[driver]: pmicWait\n");
+#endif
+
+	return;
+}
+
+/* contro CS line of SPI for PMIC communication */
+LOCAL	void	pmicCSassert(BOOL cs)
+{
+
+	return;
+}
+
+/* read PMIC register */
+EXPORT	W	pmicRead(W reg)
+{
+	W	dat;
+
+	pmicCSassert(TRUE);			/* CS assert */
+
+	pmicWait();
+
+
+	pmicCSassert(FALSE);			/* CS de-assert */
+
+	return dat;
+}
+
+/* write PMIC register */
+EXPORT	void	pmicWrite(W reg, W dat)
+{
+	pmicCSassert(TRUE);			/* CS assert */
+
+	pmicWait();
+
+	pmicWait();
+
+	pmicCSassert(FALSE);			/* CS de-assert */
+	putString("[driver]: pmicWrite\n");
+
+	return;
+}
+
+/* ------------------------------------------------------------------------ */
+
+/* basic system set up (performed during reset, and Disk Boot) */
+EXPORT	void	resetSystem(W boot)
+{
+	MEMSEG	*mp;
+	UW	i, va;
+
+	printk("%s:%s\n", __FILE__, __func__);
+	return ;
+
+        /* obtain DipSw status */
+	if (!boot) DipSw = DipSwStatus();
+
+	DisCacheMMU();
+
+        /* set up interrupt controller */
+	/* CPU: all interrupts disabled */
+	/* DSP: all interrupts disabled */
+	/* CPU: FIQ disabled */
+
+	/* GPIO: interrupt disabled */
+	/* GPIO: FIQ interrupt disabled */
+	/* internal interrupt disabled */
+	/* inhibit interrupt polarity inversion */
+	/* CPU: GPIO interrupt enabled */
+
+        /* power on controller initialization */
+	pmicInit();
+
+        /* USB power on */
+	usbPower(TRUE);
+
+        /* clear system common area (vector table, and SysInfo) */
+	memset(&SCInfo, 0, sizeof(SysCommonInfo));
+	memset(SCArea, 0, sizeof(SysCommonArea));
+
+        /* if monitor is loaded into RAM, exclude the RAM area */
+	mp = MemArea(MSA_OS, 1);
+	va = (UW)&__loadaddr;
+	if (va >= mp->top && va < mp->end) mp->end = va;
+
+        /* exclude the area where ROM disk data is stored */
+	va = (UW)ROMInfo->userarea;
+	if (va >= mp->top && va < mp->end) mp->end = va;
+
+        /* initialize system common information (SysInfo) */
+	SCInfo.ramtop = (void*)mp->top;
+	if (va < mp->top || va > mp->end) va = mp->end;
+	SCInfo.ramend = (void*)va;
+
+        /* set up EIT vectors */
+        /* we do not need _defaultHdr absolutely, but just in case set it up */
+	SCArea->intvec[EIT_DEFAULT]	= _defaultHdr;	/* default handler */
+	SCArea->intvec[EIT_UNDEF]	= _defaultHdr;	/* undefined instruction */
+	SCArea->intvec[SWI_MONITOR]	= _defaultHdr;	/* SWI - monitor SVC */
+	SCArea->intvec[EIT_IRQ(26)]	= _gio6Hdr;	/* GPIO branch */
+	SCArea->intvec[EIT_IRQ(27)]	= _gio7Hdr;
+	SCArea->intvec[EIT_IRQ(50)]	= _gio0Hdr;
+	SCArea->intvec[EIT_IRQ(51)]	= _gio1Hdr;
+	SCArea->intvec[EIT_IRQ(52)]	= _gio2Hdr;
+	SCArea->intvec[EIT_IRQ(53)]	= _gio3Hdr;
+	SCArea->intvec[EIT_IRQ(79)]	= _gio4Hdr;
+	SCArea->intvec[EIT_IRQ(80)]	= _gio5Hdr;
+	SCArea->intvec[EIT_GPIO(8)]	= _defaultHdr;	/* abort switch */
+
+        /* set up initial page table */
+	for (i = 0; i < N_MemSeg; ++i) {
+		mp = &MemSeg[i];
+		if (!mp->pa) continue;
+
+                /* FlashROM has already been mapped, and so do not touch it */
+		if (mp->attr == MSA_FROM) continue;
+
+                /* set up in unit of section (1MB) */
+		for ( va  = (mp->top & 0xfff00000);
+		      va != ((mp->end + 0x000fffff) & 0xfff00000);
+		      va += 0x00100000 ) {
+			TopPageTable[va / 0x00100000] =
+				((mp->pa & 0xfff00000) + va) |
+				 (mp->pa & 0x000fffff);
+		}
+	}
+
+	for (i = 0; i < N_NoMemSeg; ++i) {
+		mp = &NoMemSeg[i];
+
+                /* set up in unit of section (1MB) */
+		for ( va  = (mp->top & 0xfff00000);
+		      va != ((mp->end + 0x000fffff) & 0xfff00000);
+		      va += 0x00100000 ) {
+			TopPageTable[va / 0x00100000] = 0;
+		}
+	}
+
+	DSB();
+	/* I/D TLB invalidate */
+	/* invalidate BTC */
+	DSB();
+	ISB();
+
+	EnbCacheMMU();
+
+	return;
+}
+
+/* ------------------------------------------------------------------------ */
+
+/*
+        system termination: reset / system power off
+        reset   0       power off
+                -1      reboot
+                0xaa55  halt boot and power off
+*/
+EXPORT	void	sysExit(W reset)
+{
+	DisCacheMMU();
+
+        /* after this point, delay such as waitUsec() spends more time than the number indicates // */
+
+	/* LED off */
+	cpuLED(0x00);
+
+        /* all interrupts disabled */
+	/* CPU: all interrupts disabled */
+	/* DSP: all interrupts disabled */
+	/* FIQ disabled */
+	/* internal interrupt disabled */
+        /* power on controller initialization */
+	pmicInit();
+
+        /* USB power off */
+	usbPower(FALSE);
+
+	if (reset >= 0) powerOff();
+
+        /* reset start */
+	resetStart();
+}
+
+/* ------------------------------------------------------------------------ */
